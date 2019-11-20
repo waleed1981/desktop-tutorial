bcca8ac2739d7f122b4a62c2aae68d38b548a506
osku <osku@ICT.local>
2019-11-20 10:05:08 +0200

    20112019

---
+++ eduvantaa-master_uusi/index4.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $("#kuva1").click(function(){
+      $("#panel1").slideToggle(1000);
+    });
+});
+$(document).ready(function(){
+    $("#kuva2").click(function(){
+      $("#panel2").slideToggle(1000);
+    });
+});
+
+
+
