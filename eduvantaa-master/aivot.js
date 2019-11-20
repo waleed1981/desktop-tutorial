f44330e370029d46d168a50ef2020b2f4fb48333
osku <osku@ICT.local>
2019-11-20 13:00:41 +0200

    20112019

--- eduvantaa-master/aivot.js
+++ eduvantaa-master/aivot.js
@@ -35,32 +35,45 @@ function yhteystietoFunktio() {
     document.getElementById("yhteystiedotTuloste").innerHTML = text;
   }
   function diagrammiFunktio(){
-    // etsitään harjoitus2.html sivulta elementti, jonka id=myCanvas
     var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
+    var arvoArray;
+    var arvoPituus;
+    var piste=40;
+    var diagrammi = c.getContext("2d");
     
-    // diagrammiin voidaan kirjoittaa numeroita haluttuihin kohtiin.
-    ctx.fillText("10", 0, 200);
-    ctx.fillText("20", 40, 250);
-    ctx.fillText("30", 60, 200);
-    ctx.fillText("40", 80, 210);
-    ctx.fillText("50", 100, 250);
-    ctx.fillText("60", 120, 270);
-    ctx.fillText("70", 140, 290);
-    ctx.fillText("80", 300, 320);
-    ctx.fillText("90", 325, 300);
+    arvoArray = [100, 150, 100, 110, 120, 110, 120, 120, 130, 150, +
+        100, 150, 100, 110, 120, 110, 420, 320, 430, 150, +
+        150, 100, 150, 100, 110, 120, 110, 120, 120, 130, 150];
+       
+    arvoPituus= arvoArray.length;
+    
+    diagrammi.beginPath();
+    diagrammi.strokeStyle = "blue";
 
-    
-    // Alla olevalla koodilla voidaan piirtää viivaa diagrammiin.
-    ctx.moveTo(40, 200);
-    ctx.lineTo(60, 250);
-    ctx.lineTo(80, 200);
-    ctx.lineTo(100, 210);
-    ctx.lineTo(120, 250);
-    ctx.lineTo(140, 270);
-    ctx.lineTo(300, 290);
-    ctx.lineTo(325, 320);
-    ctx.stroke();
+    diagrammi.moveTo(piste, 500-arvoArray[0]);
+    diagrammi.fillText(arvoArray[0], piste, 500-arvoArray[0]);
+        
+    for (i = 1; i < arvoPituus; i++) {
+        piste=piste+20;
+        diagrammi.lineTo(piste, 500-arvoArray[i]);
+        diagrammi.fillText(arvoArray[i], piste, 500-arvoArray[i]);
     }
-    
+    diagrammi.stroke();
+
+    diagrammi.fillText("50", 0, 450);
+    diagrammi.fillText("100", 0, 400);
+    diagrammi.fillText("150", 0, 350);
+    diagrammi.fillText("200", 0, 300);
+    diagrammi.fillText("250", 0, 250);
+    diagrammi.fillText("300", 0, 200);
+    diagrammi.fillText("350", 0, 150);
+    diagrammi.fillText("400", 0, 100);
+    diagrammi.fillText("450", 0, 50);
+
+    diagrammi.fillText("5.1.", 120, 500);
+    diagrammi.fillText("10.1.", 220, 500);
+    diagrammi.fillText("15.1.", 320, 500);
+    diagrammi.fillText("20.1.", 420, 500);
+    diagrammi.fillText("25.1.", 520, 500);
+    diagrammi.fillText("30.1.", 620, 500);
+}
